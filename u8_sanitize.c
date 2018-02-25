938a7b2ddfc53b3feda38a89044d920b1012a054
Dmitriy Kalugin-Balashov <rvncerr@rvncerr.org>
2018-02-25 01:05:19 -0800

    u8_find_rune.

--- u8_sanitize.c
+++ u8_sanitize.c
@@ -2,21 +2,21 @@
 
 void u8_sanitize(char *s, int how) {
 	char *p = s;
-	void (*rune_length)(char*);
+	int (*f_rune_length)(const char*);
 	
 	switch(how) {
 	case U8_SANITIZE_UTF8:
-		rune_length = u8_rune_length;
+		f_rune_length = u8_rune_length;
 		break;
 	case U8_SANITIZE_XML:
-		rune_length = u8_rune_length_xml;
+		f_rune_length = u8_rune_length_xml;
 		break;
 	default:
 		return;
 	}
 
 	for(;;) {
-		int rune_length = rune_length(p);
+		int rune_length = f_rune_length(p);
 		if(rune_length > 0) p += rune_length;
 		else if(rune_length == 0) break;
 		else { *p = ' '; p++; }
